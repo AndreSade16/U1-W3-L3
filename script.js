8445a5f711f17d77be19fbb4105f72e2d86c337d
Andrea Saderi <andrea.saderi16@gmail.com>
2026-03-18 15:10:05 +0100

    Adds index.html, scripts.js, assets folder

---
+++ script.js
@@ -0,0 +1,37 @@
+const input = document.getElementById("text-input");
+const addBtn = document.getElementById("add-btn");
+const listContainer = document.getElementById("list-container");
+const removeBtns = document.querySelectorAll(".remove-task-btn");
+const checkboxes = document.querySelectorAll(".checkbox");
+const form = document.querySelector("form");
+
+const appendNewTask = (e) => {
+  e.preventDefault();
+  if (!input.value) {
+    alert("Inserisci del testo");
+    return;
+  }
+  const newTask = document.createElement("div");
+  newTask.classList.add("task-container");
+  newTask.innerHTML = `
+    <input type="checkbox" class="checkbox" onchange="lineThrough(event)"/>
+    <p class="task-p">${input.value}</p>
+    <button class="remove-task-btn" onclick="removeTask(event)"><i class="fas fa-trash" onclick="removeTaskIcon(event)"></i></button>
+    `;
+  listContainer.appendChild(newTask);
+  form.reset();
+};
+
+const removeTask = (e) => {
+  e.target.parentElement.remove();
+};
+
+const removeTaskIcon = (e) => {
+  e.target.parentElement.parentElement.remove();
+};
+
+const lineThrough = (e) => {
+  e.target.parentElement.classList.toggle("checked");
+};
+
+addBtn.addEventListener("click", appendNewTask);
